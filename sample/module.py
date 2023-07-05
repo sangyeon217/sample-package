4c15e8ee2393ebc703cac2be1595d236c2cc902d
sangyeon217 <sangyeon217@gmail.com>
2023-07-05 23:09:40 +0900

    headless 모드로 동작하도록 수정

--- sample/module.py
+++ sample/module.py
@@ -4,7 +4,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def open_google_web():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=webdriver.ChromeOptions())
+    chrome_options = webdriver.ChromeOptions()
+
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     try:
         driver.get("https://www.google.com/")
     finally:
